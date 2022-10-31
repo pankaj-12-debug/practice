d4e220acb4a0a7c6ee460cdba8d2c13162e78af5
pankaj-12-debug <kajopkkumar12@gmail.com>
2022-11-01 00:06:21 +0530

    sql-sequelize-learn

---
+++ util/database.js
@@ -0,0 +1,14 @@
+//const mysql = require('mysql2');
+//const pool = mysql.createPool({
+    //host: 'localhost',
+    //user: 'root',
+    //database: 'node-complete',
+  //  password: 'Meena@123'
+//});
+//module.exports = pool.promise();
+const Sequelize=require('sequelize');
+const sequelize=new Sequelize('node-complete','root','Meena@123',{
+    dialect:'mysql',
+    host:'localhost'
+});
+module.exports=sequelize;
